0438ba763963405993396e372fa7516b62e5a42c
KazaBen <benas.kaz@gmail.com>
2016-12-04 22:47:01 +0000

    Site is completed left word files added folders

--- JavaScript/puzzle.js
+++ JavaScript/puzzle.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var differenceFound = [false, false, false, false, false]; // Using boolean array to have store information if the difference was found
-var puzzleImage = "spongebob.jpg"; // Choosing first puzzle
+var puzzleImage = "./Pictures/spongebob.jpg"; // Choosing first puzzle
 
 // Declaring variables how many differences left to find and how many found
 var toFind = 5;
@@ -63,79 +63,79 @@ function checkDifference(evt) {
 	var pos = getMouseXY(evt); // Getting mouse click position
 
 	/* Checking if the clicked area is where the difference is, checking if that difference is not found, checking if it is the right image*/
-	if (pos.x >= 230 && pos.y >= 140 && pos.x <= 290 && pos.y <= 180 && differenceFound[0] == false && puzzleImage == "spongebob.jpg") {
+	if (pos.x >= 230 && pos.y >= 140 && pos.x <= 290 && pos.y <= 180 && differenceFound[0] == false && puzzleImage == "./Pictures/spongebob.jpg") {
 		differenceFound[0] = true; // Difference was found so making it true
 		foundADifference(); // Calling function foundADifference
 		drawCircle(260, 161, 560, 161, 25, 'white'); // Drawing circle around the place where the function was found
 	}
 
 	// Same structure for the following if statements as in line 45
-	if (pos.x >= 230 && pos.y >= 70 && pos.x <= 260 && pos.y <= 100 && differenceFound[1] == false && puzzleImage == "spongebob.jpg") {
+	if (pos.x >= 230 && pos.y >= 70 && pos.x <= 260 && pos.y <= 100 && differenceFound[1] == false && puzzleImage == "./Pictures/spongebob.jpg") {
 		differenceFound[1] = true;
 		foundADifference();
 		drawCircle(245, 90, 545, 90, 20, 'green');
 	}
-	if (pos.x >= 160 && pos.y >= 120 && pos.x <= 190 && pos.y <= 150 && differenceFound[2] == false && puzzleImage == "spongebob.jpg") {
+	if (pos.x >= 160 && pos.y >= 120 && pos.x <= 190 && pos.y <= 150 && differenceFound[2] == false && puzzleImage == "./Pictures/spongebob.jpg") {
 		differenceFound[2] = true;
 		foundADifference();
 		drawCircle(174, 135, 474, 135, 15, 'black');
 	}
-	if (pos.x >= 260 && pos.y >= 15 && pos.x <= 290 && pos.y <= 45 && differenceFound[3] == false && puzzleImage == "spongebob.jpg") {
+	if (pos.x >= 260 && pos.y >= 15 && pos.x <= 290 && pos.y <= 45 && differenceFound[3] == false && puzzleImage == "./Pictures/spongebob.jpg") {
 		differenceFound[3] = true;
 		foundADifference();
 		drawCircle(271, 24, 571, 24, 20, 'blue');
 	}
-	if (pos.x >= 0 && pos.y >= 0 && pos.x <= 30 && pos.y <= 30 && differenceFound[4] == false && puzzleImage == "spongebob.jpg") {
+	if (pos.x >= 0 && pos.y >= 0 && pos.x <= 30 && pos.y <= 30 && differenceFound[4] == false && puzzleImage == "./Pictures/spongebob.jpg") {
 		differenceFound[4] = true;
 		foundADifference();
 		drawCircle(20, 14, 325, 14, 20, 'yellow');
 	}
-	if (pos.x >= 0 && pos.y >= 215 && pos.x <= 30 && pos.y <= 245 && differenceFound[0] == false && puzzleImage == "squidward.jpg") {
+	if (pos.x >= 0 && pos.y >= 215 && pos.x <= 30 && pos.y <= 245 && differenceFound[0] == false && puzzleImage == "./Pictures/squidward.jpg") {
 		differenceFound[0] = true;
 		foundADifference();
 		drawCircle(15, 231, 320, 231, 20, 'yellow');
 	}
-	if (pos.x >= 0 && pos.y >= 0 && pos.x <= 10 && pos.y <= 30 && differenceFound[1] == false && puzzleImage == "squidward.jpg") {
+	if (pos.x >= 0 && pos.y >= 0 && pos.x <= 10 && pos.y <= 30 && differenceFound[1] == false && puzzleImage == "./Pictures/squidward.jpg") {
 		differenceFound[1] = true;
 		foundADifference();
 		drawCircle(5, 10, 320, 15, 20, 'white');
 	}
-	if (pos.x >= 180 && pos.y >= 30 && pos.x <= 210 && pos.y <= 60 && differenceFound[2] == false && puzzleImage == "squidward.jpg") {
+	if (pos.x >= 180 && pos.y >= 30 && pos.x <= 210 && pos.y <= 60 && differenceFound[2] == false && puzzleImage == "./Pictures/squidward.jpg") {
 		differenceFound[2] = true;
 		foundADifference();
 		drawCircle(194, 43, 494, 43, 20, 'red');
 	}
-	if (pos.x >= 100 && pos.y >= 245 && pos.x <= 130 && pos.y <= 275 && differenceFound[3] == false && puzzleImage == "squidward.jpg") {
+	if (pos.x >= 100 && pos.y >= 245 && pos.x <= 130 && pos.y <= 275 && differenceFound[3] == false && puzzleImage == "./Pictures/squidward.jpg") {
 		differenceFound[3] = true;
 		foundADifference();
 		drawCircle(117, 257, 420, 257, 15, 'brown');
 	}
-	if (pos.x >= 80 && pos.y >= 135 && pos.x <= 110 && pos.y <= 165 && differenceFound[4] == false && puzzleImage == "squidward.jpg") {
+	if (pos.x >= 80 && pos.y >= 135 && pos.x <= 110 && pos.y <= 165 && differenceFound[4] == false && puzzleImage == "./Pictures/squidward.jpg") {
 		differenceFound[4] = true;
 		foundADifference();
 		drawCircle(95, 150, 395, 150, 20, 'black');
 	}
-	if (pos.x >= 85 && pos.y >= 200 && pos.x <= 105 && pos.y <= 230 && differenceFound[0] == false && puzzleImage == "panda.png") {
+	if (pos.x >= 85 && pos.y >= 200 && pos.x <= 105 && pos.y <= 230 && differenceFound[0] == false && puzzleImage == "./Pictures/panda.png") {
 		differenceFound[0] = true;
 		foundADifference();
 		drawCircle(91, 207, 391, 207, 20, 'yellow');
 	}
-	if (pos.x >= 115 && pos.y >= 200 && pos.x <= 145 && pos.y <= 230 && differenceFound[1] == false && puzzleImage == "panda.png") {
+	if (pos.x >= 115 && pos.y >= 200 && pos.x <= 145 && pos.y <= 230 && differenceFound[1] == false && puzzleImage == "./Pictures/panda.png") {
 		differenceFound[1] = true;
 		foundADifference();
 		drawCircle(145, 215, 445, 215, 20, 'green');
 	}
-	if (pos.x >= 200 && pos.y >= 35 && pos.x <= 230 && pos.y <= 65 && differenceFound[2] == false && puzzleImage == "panda.png") {
+	if (pos.x >= 200 && pos.y >= 35 && pos.x <= 230 && pos.y <= 65 && differenceFound[2] == false && puzzleImage == "./Pictures/panda.png") {
 		differenceFound[2] = true;
 		foundADifference();
 		drawCircle(218, 49, 518, 49, 20, 'red');
 	}
-	if (pos.x >= 145 && pos.y >= 125 && pos.x <= 175 && pos.y <= 155 && differenceFound[3] == false && puzzleImage == "panda.png") {
+	if (pos.x >= 145 && pos.y >= 125 && pos.x <= 175 && pos.y <= 155 && differenceFound[3] == false && puzzleImage == "./Pictures/panda.png") {
 		differenceFound[3] = true;
 		foundADifference();
 		drawCircle(163, 140, 464, 140, 24, 'blue');
 	}
-	if (pos.x >= 150 && pos.y >= 160 && pos.x <= 180 && pos.y <= 190 && differenceFound[4] == false && puzzleImage == "panda.png") {
+	if (pos.x >= 150 && pos.y >= 160 && pos.x <= 180 && pos.y <= 190 && differenceFound[4] == false && puzzleImage == "./Pictures/panda.png") {
 		differenceFound[4] = true;
 		foundADifference();
 		drawCircle(167, 173, 467, 173, 20, 'brown');
@@ -156,12 +156,12 @@ function checkDifference(evt) {
 function changePuzzle() {
 
 	//Changing puzzles in a sequence
-	if (puzzleImage == "spongebob.jpg") {
-		puzzleImage = "squidward.jpg"
-	} else if (puzzleImage == "squidward.jpg") {
-		puzzleImage = "panda.png"
-	} else if (puzzleImage == "panda.png") {
-		puzzleImage = "spongebob.jpg"
+	if (puzzleImage == "./Pictures/spongebob.jpg") {
+		puzzleImage = "./Pictures/squidward.jpg"
+	} else if (puzzleImage == "./Pictures/squidward.jpg") {
+		puzzleImage = "./Pictures/panda.png"
+	} else if (puzzleImage == "./Pictures/panda.png") {
+		puzzleImage = "./Pictures/spongebob.jpg"
 	}
 
 	draw(context, puzzleImage); // Drawing the puzzle
