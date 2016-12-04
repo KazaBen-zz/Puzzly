84b4f30e26deeb18781c7f3de85fe17dc1e915bf
KazaBen <benas.kaz@gmail.com>
2016-12-04 16:39:34 +0000

    Founders.html is complete with comments, indentation

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
-http://www.w3schools.com/howto/howto_js_topnav.asp
-function myFunction() {
+// Used http://www.w3schools.com/howto/howto_js_topnav.asp as an example
+function navigation() {
 	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
+	if (x.className == "topnav") {
 		x.className += " responsive";
 	} else {
 		x.className = "topnav";
