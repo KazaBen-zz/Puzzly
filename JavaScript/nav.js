c523c23b5b02852e78c20142cc1718b34b20ba64
KazaBen <benas.kaz@gmail.com>
2016-12-07 21:22:47 +0000

    Fixed bugs, sorted out responsive nav. Need comments, indent, valid

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,9 +1,12 @@
-// Used http://www.w3schools.com/howto/howto_js_topnav.asp as an example
 function navigation() {
-	var x = document.getElementById("myTopnav");
+	var x = document.getElementById("myTopnav"); /*Getting navigation element */
+	
+	/* If classname =="topnav" making it "responsive" needed for CSS */
 	if (x.className == "topnav") {
-		x.className += " responsive";
-	} else {
+		x.className = "responsive";
+	}
+	/* If the className=="responsive" means button was clicked before and is in "responsive" state and CSS to make it dropdown is being executed and the button is clicked again so we are resetting className to "topnav" to turn off drop down display*/
+	else if(x.className == "responsive"){
 		x.className = "topnav";
 	}
 }
